ba7bc750e074bc9dbe7922847ce5c71e42893a21
Nayeem Hasan <hasannayeeem@gmail.com>
2022-08-30 01:41:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
+import { Button } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ChatPage from './pages/ChatPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <p>bismillah</p>
+      <Button colorScheme='blue'>Button</Button>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/chats' element={<ChatPage />} />
+      </Routes>
     </div>
   );
 }
